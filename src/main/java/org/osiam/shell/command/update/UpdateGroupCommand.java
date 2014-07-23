92a5e625f73bf1d53cdda687af825bac0479d0b6
rainu <rainu@raysha.de>
2014-07-23 20:58:37 +0200

    implements powerfully update group subshell

--- src/main/java/org/osiam/shell/command/update/UpdateGroupCommand.java
+++ src/main/java/org/osiam/shell/command/update/UpdateGroupCommand.java
@@ -1,5 +1,7 @@
 package org.osiam.shell.command.update;
 
+import java.io.IOException;
+
 import org.osiam.client.OsiamConnector;
 import org.osiam.client.oauth.AccessToken;
 import org.osiam.client.query.Query;
@@ -8,41 +10,69 @@ import org.osiam.resources.scim.Group;
 import org.osiam.resources.scim.SCIMSearchResult;
 import org.osiam.resources.scim.UpdateGroup;
 import org.osiam.resources.scim.User;
+import org.osiam.shell.command.AbstractBuilderCommand;
 
+import de.raysha.lib.jsimpleshell.Shell;
+import de.raysha.lib.jsimpleshell.ShellBuilder;
+import de.raysha.lib.jsimpleshell.ShellDependent;
 import de.raysha.lib.jsimpleshell.annotation.Command;
 import de.raysha.lib.jsimpleshell.annotation.Param;
+import de.raysha.lib.jsimpleshell.io.OutputBuilder;
+import de.raysha.lib.jsimpleshell.io.OutputDependent;
 
 /**
  * This class contains commands that can update groups.
  * 
  * @author rainu
  */
-public class UpdateGroupCommand {
+public class UpdateGroupCommand implements ShellDependent, OutputDependent {
 	private AccessToken accessToken;
 	private final OsiamConnector connector;
 
+	private Shell shell;
+	private OutputBuilder output;
+	
 	public UpdateGroupCommand(AccessToken at, OsiamConnector connector) {
 		this.accessToken = at;
 		this.connector = connector;
 	}
 	
-	@Command(description = "Add the given user to the given group.")
-	public Object addMemberToGroup(
-			@Param(name = "userName", description = "The name of the user.")
-			String userName,
+	@Override
+	public void cliSetShell(Shell theShell) {
+		this.shell = theShell;
+	}
+	
+	@Override
+	public void cliSetOutput(OutputBuilder output) {
+		this.output = output;
+	}
+	
+	@Command(description = "Update the given group.")
+	public Object updateGroup(
 			@Param(name = "groupName", description = "The name of the group.")
-			String groupName){
+			String groupName) throws IOException{
 		
 		final Group group = getGroup(groupName);
 		if(group == null) return "There is no group with the name \"" + groupName + "\"!";
 		
-		final User user = getUser(userName);
-		if(user == null) return "There is no user with the name \"" + userName + "\"!";
+		final UpdateGroupBuilder builder = new UpdateGroupBuilder(group);
 		
-		final UpdateGroup update = new UpdateGroup.Builder().addMember(user.getId()).build();
+		final Shell subShell = ShellBuilder.subshell("update-group", shell)
+									.addHandler(builder)
+								.build();
+		
+		output.out()
+			.normal("In this subshell you can edit the group. Leave this sub shell via \"commit\" to persist the changes.")
+		.println();
+		
+		subShell.commandLoop();
+		
+		final UpdateGroup update = builder.build();
+		if(update == null) return null;
 		
 		return connector.updateGroup(group.getId(), update, accessToken);
 	}
+	
 
 	protected Group getGroup(String groupName) {
 		final Query query = new QueryBuilder().filter("displayName eq \"" + groupName + "\"").build();
@@ -55,14 +85,85 @@ public class UpdateGroupCommand {
 		return null;
 	}
 	
-	private User getUser(String userName) {
-		final Query query = new QueryBuilder().filter("userName eq \"" + userName + "\"").build();
-		final SCIMSearchResult<User> result = connector.searchUsers(query, accessToken);
-
-		if (result.getTotalResults() > 0) {
-			return result.getResources().get(0);
+	public class UpdateGroupBuilder extends AbstractBuilderCommand<UpdateGroup> {
+		private final Group group;
+		private UpdateGroup.Builder builder = new UpdateGroup.Builder();
+		
+		public UpdateGroupBuilder(Group group) {
+			this.group = group;
+		}
+		
+		@Command(description = "Shows the current (persited) group that will be updated.")
+		public Group showGroup(){
+			return group;
+		}
+		
+		@Command(description = "Shows the group state. This state is not persisted yet!")
+		public Group showState(){
+			return build().getScimConformUpdateGroup();
+		}
+		
+		@Command(description = "Add the given user to the given group.")
+		public String addUser(
+				@Param(name = "userName", description = "The name of the user.")
+				String userName){
+			
+			final User user = getUser(userName);
+			if(user == null) return "There is no user with the name \"" + userName + "\"!";
+			
+			builder.addMember(user.getId());
+			
+			return null;
+		}
+		
+		@Command(description = "Remove the given user from the given group.")
+		public String removeUser(
+				@Param(name = "userName", description = "The name of the user.")
+				String userName){
+			
+			final User user = getUser(userName);
+			if(user == null) return "There is no user with the name \"" + userName + "\"!";
+			
+			builder.deleteMember(user.getId());
+			
+			return null;
+		}
+		
+		@Command(description = "Remove all members from the given group.")
+		public void removeAll(){
+			builder.deleteMembers();
+		}
+		
+		@Command(description = "Remove the given user from the given group.")
+		public void rename(
+				@Param(name = "name", description = "The new group display name.")
+				String newName){
+			
+			builder.updateDisplayName(newName);
 		}
 
-		return null;
+		@Command(description = "Set the externalId for the group.")
+		public void setExternalId(
+				@Param(name = "externalId", description = "The new externalId for the group.")
+				String externalId){
+			
+			builder.updateExternalId(externalId);
+		}
+		
+		private User getUser(String userName) {
+			final Query query = new QueryBuilder().filter("userName eq \"" + userName + "\"").build();
+			final SCIMSearchResult<User> result = connector.searchUsers(query, accessToken);
+
+			if (result.getTotalResults() > 0) {
+				return result.getResources().get(0);
+			}
+
+			return null;
+		}
+		
+		@Override
+		protected UpdateGroup _build() {
+			return builder.build();
+		}
 	}
 }
